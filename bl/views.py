8327310359385b9a6b83d6dac0e37c45b8e1a0b5
wo <wojing0000@163.com>
2018-06-29 01:10:53 +0800

    add pagnitation

--- bl/views.py
+++ bl/views.py
@@ -5,7 +5,17 @@ from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 # Create your views here.
 def index(request):
-    ablum_list = get_list_or_404(Ablum)[-10:]
+    ablum = get_list_or_404(Ablum)
+    pagniator = Paginator(ablum,15)
+    page = request.GET.get('page')
+
+    try:
+        ablum_list = pagniator.page(page)
+    except PageNotAnInteger:
+        ablum_list = pagniator.page(1)
+    except EmptyPage:
+        ablum_list = pagniator.page(pagniator.num_pages)
+
    # ab_json = json.load(serializers.serialize("json",ablum_list))
     return  render(request,"bl/index.html",{ "ablum_list": ablum_list })
 
@@ -13,7 +23,14 @@ def index(request):
 def detail(request,ablum_id):
     ablum = Ablum.objects.filter(id = ablum_id)[0]
     pic_list =  ablum.pic_set.all()
-    paginator = Paginator(pic_list,10)
-    page = paginator.page(2)
+    paginator = Paginator(pic_list,3)
+    page = request.GET.get("page")
 
-    return render(request,"bl/detail.html/",{"ablum":ablum,"pic_list": page})
+    try:
+        pic_list = paginator.page(page)
+    except PageNotAnInteger:
+        pic_list = paginator.page(1)
+    except EmptyPage:
+        pic_list = paginator.page(paginator.num_pages)
+
+    return render(request,"bl/detail.html",{"ablum":ablum,"pic_list": pic_list})
